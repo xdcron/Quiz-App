47697a4eabb770f705d8566f0ae34d2d88bc3e90
xdcron <xdcron95@gmail.com>
2023-09-20 11:48:28 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,144 @@
+import { useState } from "react";
+import quizQuestions from "./questions";
+
+export default function App() {
+  const [startQuiz, setStartQuiz] = useState(false);
+
+  function handleStartQuiz() {
+    setStartQuiz(true);
+  }
+
+  return (
+    <div className="container">
+      <Heading />
+      {!startQuiz && <StartQuiz onStartQuiz={handleStartQuiz} />}
+      {startQuiz && <Question setStartQuiz={setStartQuiz} />}
+    </div>
+  );
+}
+
+function Heading() {
+  return <h1>THE GREATES QUIZ EVER</h1>;
+}
+
+function Question({ setStartQuiz }) {
+  const [question, setQuestion] = useState(quizQuestions[0]);
+  const [answer, setAnswer] = useState(null);
+  const [score, setScore] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+  const isCorrect = answer === question.answer;
+
+  function handleNextQuestion(e) {
+    e.preventDefault();
+    if (!answer) return;
+    setQuestion(quizQuestions[question.questionNumber]);
+    isCorrect ? setScore((scr) => scr + 1) : setScore(score);
+    console.log(score);
+    console.log(question);
+    setAnswer(null);
+  }
+
+  function handleSkip() {
+    setQuestion(quizQuestions[question.questionNumber]);
+  }
+
+  function handleFinish() {
+    if (question.questionNumber === quizQuestions.length) {
+      isCorrect ? setScore((scr) => scr + 1) : setScore(score);
+      setShowResults(true);
+    }
+  }
+
+  function handleReset() {
+    setStartQuiz(false);
+    setQuestion(quizQuestions[0]);
+    setAnswer(null);
+    setScore(0);
+    setShowResults(false);
+  }
+
+  function handleMessage() {
+    if (score > quizQuestions.length / 2 && score < quizQuestions.length)
+      return "Not Bad at all 😌🧐🦁";
+    if (score < quizQuestions.length / 2)
+      return "better luck next time 😢😭😥🤡";
+    if (score === quizQuestions.length)
+      return "WOW Amazing, Is this Elon MusK? 😲😲🧠🧠🦾";
+  }
+
+  return (
+    <form onSubmit={handleNextQuestion}>
+      {showResults ? (
+        <div className="start">
+          <p>{`You got ${score} / ${quizQuestions.length}`}</p>
+          <p>{handleMessage()}</p>
+          <p>
+            <strong>Good job on Completing the quiz.</strong>
+          </p>
+
+          <Button onClick={handleReset}>Try Again?</Button>
+        </div>
+      ) : (
+        <>
+          <h2>
+            {question.questionNumber}. {question.question}
+          </h2>
+          <div className="options">
+            {question.answerlist.map((que) => (
+              <Options
+                key={que}
+                answer={answer}
+                onAnswer={setAnswer}
+                option={que}
+              />
+            ))}
+          </div>
+
+          <div className="buttons">
+            {question.questionNumber < quizQuestions.length && (
+              <Button onClick={handleSkip}>Skip</Button>
+            )}
+            {question.questionNumber < quizQuestions.length && (
+              <Button onClick={handleReset}>Restart?</Button>
+            )}
+            <Button onClick={handleFinish}>
+              {question.questionNumber < quizQuestions.length
+                ? "Next"
+                : "Finish"}
+            </Button>
+          </div>
+        </>
+      )}
+    </form>
+  );
+}
+
+function Options({ option, onAnswer }) {
+  function handleChoice() {
+    onAnswer(option);
+  }
+  return (
+    <label>
+      <input
+        type="radio"
+        onChange={handleChoice}
+        name={"question"}
+        value={option}
+      />
+      {option}
+    </label>
+  );
+}
+
+function StartQuiz({ onStartQuiz }) {
+  return (
+    <div className="start">
+      <h2>Take this Exciting quiz</h2>
+      <Button onClick={onStartQuiz}>Start Quiz</Button>
+    </div>
+  );
+}
+
+function Button({ children, onClick }) {
+  return <button onClick={onClick}>{children}</button>;
+}
---
+++ src/questions.js
@@ -0,0 +1,122 @@
+const quizQuestions = [
+  {
+    questionNumber: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+    answerlist: ["Paris", "Tokyo", "Miami", "Katutura"],
+  },
+  {
+    questionNumber: 2,
+    question: "What is the chemical symbol for water?",
+    answer: "H2O",
+    answerlist: ["CO2", "H2O", "NH3", "CH4"],
+  },
+  {
+    questionNumber: 3,
+    question: "Who was the first President of the United States?",
+    answer: "George Washington",
+    answerlist: [
+      "Benjamin Franklin",
+      "Thomas Jefferson",
+      "George Washington",
+      "Abraham Lincoln",
+    ],
+  },
+  {
+    questionNumber: 4,
+    question: "Which river is the longest in the world?",
+    answer: "The Nile River",
+    answerlist: [
+      "Amazon River",
+      "Mississippi River",
+      "The Nile River",
+      "Danube River",
+    ],
+  },
+  {
+    questionNumber: 5,
+    question:
+      "What is the highest-grossing film of all time (as of my knowledge cutoff date in September 2021)?",
+    answer: "Avatar",
+    answerlist: [
+      "Titanic",
+      "The Lord of the Rings: The Return of the King",
+      "Avengers: Endgame",
+      "Avatar",
+    ],
+  },
+  {
+    questionNumber: 6,
+    question: "Who is known as the King of Pop?",
+    answer: "Michael Jackson",
+    answerlist: ["Elvis Presley", "Michael Jackson", "Justin Bieber", "Prince"],
+  },
+  {
+    questionNumber: 7,
+    question: "In which sport is the World Series a championship event?",
+    answer: "Baseball",
+    answerlist: ["Football (Soccer)", "Tennis", "Baseball", "Golf"],
+  },
+  {
+    questionNumber: 8,
+    question: "Who wrote the novel To Kill a Mockingbird?",
+    answer: "Harper Lee",
+    answerlist: ["J.K. Rowling", "Mark Twain", "Charles Dickens", "Harper Lee"],
+  },
+  {
+    questionNumber: 9,
+    question: "What does the acronym CPU stand for in computing?",
+    answer: "Central Processing Unit",
+    answerlist: [
+      "Computer Processing Unit",
+      "Central Processing Unit",
+      "Central Programming Unit",
+      "Central Peripheral Unit",
+    ],
+  },
+  {
+    questionNumber: 10,
+    question:
+      "Which fruit is known as the king of fruits and is famous for its strong odor?",
+    answer: "Durian",
+    answerlist: ["Strawberry", "Durian", "Banana", "Apple"],
+  },
+  {
+    questionNumber: 11,
+    question: "What is the name of the largest planet in our solar system?",
+    answer: "Jupiter",
+    answerlist: ["Jupiter", "Mars", "Venus", "Saturn"],
+  },
+  {
+    questionNumber: 12,
+    question: "Who is the Greek god of thunder and lightning?",
+    answer: "Zeus",
+    answerlist: ["Apollo", "Hermes", "Zeus", "Poseidon"],
+  },
+  {
+    questionNumber: 13,
+    question: "Who painted the Mona Lisa?",
+    answer: "Leonardo da Vinci",
+    answerlist: [
+      "Michelangelo",
+      "Vincent van Gogh",
+      "Leonardo da Vinci",
+      "Pablo Picasso",
+    ],
+  },
+  {
+    questionNumber: 14,
+    question: "How many official languages does the United Nations have?",
+    answer: "6",
+    answerlist: ["4", "12", "6", "8"],
+  },
+  {
+    questionNumber: 15,
+    question:
+      "Which TV series features a group of friends living in New York City and hanging out at Central Perk?",
+    answer: "Friends",
+    answerlist: ["How I Met Your Mother", "The Office", "Seinfeld", "Friends"],
+  },
+];
+
+export default quizQuestions;
